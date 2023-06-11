57b6a33d06d03cc1f828ca0725b9bc1f31592829
AugetyVolta <1193040110@qq.com>
2023-06-11 17:32:56 +0800

    lab2

---
+++ include/debugk.h
@@ -0,0 +1,11 @@
+#ifndef _DBGK_H_
+#define _DBGK_H_
+#include <printk.h>
+#define DEBUGK // 可注释
+#ifdef DEBUGK
+#define DEBUGK(fmt, ...) do { printk("debugk::" fmt, ##__VA_ARGS__); } while (0)
+#else
+#define DEBUGK(...)
+#endif
+#endif // !_DBGK_H_
+
